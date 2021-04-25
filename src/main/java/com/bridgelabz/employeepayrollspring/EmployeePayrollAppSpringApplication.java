ed01f3af606bfb85d00152506ff7b1b0caafbdb1
tejas181995 <tejasdev95@gmail.com>
2021-04-26 01:21:33 +0530

    ADD. created EmployeePayroll database

---
+++ src/main/java/com/bridgelabz/employeepayrollspring/EmployeePayrollAppSpringApplication.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.employeepayrollspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeePayrollAppSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeePayrollAppSpringApplication.class, args);
+	}
+
+}
